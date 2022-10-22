7a7efade77294316d14819ea7a21f13930819f82
unknown <sand8594@hufs.ac.kr>
2022-10-22 15:28:53 +0900

    selector

--- kanban-board/src/App.tsx
+++ kanban-board/src/App.tsx
@@ -1,7 +1,23 @@
+import React from "react";
+import { useRecoilState} from "recoil";
+import { hourSelector, minuteState } from "./atom";
+
 function App() {
+  const [minutes, setMinutes] = useRecoilState(minuteState)
+  const [hours, setHours] = useRecoilState(hourSelector);
+
+  const onMinutesChange = (e:React.FormEvent<HTMLInputElement>) => {
+    setMinutes(+e.currentTarget.value); 
+    // string값을 number로 바꾸고 싶으면 앞에 +
+    // input은 항상 string을 value로 반환함
+  }
+  const onHoursChange = (e:React.FormEvent<HTMLInputElement>) => {
+    setHours(+e.currentTarget.value); 
+  }
   return (
     <div>
-      
+      <input value = {minutes} onChange={onMinutesChange} type="number" placeholder="Minutes"/>
+      <input value = {hours} onChange={onHoursChange} type="number" placeholder="Seconds"/>
     </div>
   );
 }
